c7ebfc26afbfb76ba55bf311ef58133d0544ceaf
ys0uyno <ys0uyno@163.com>
2019-02-11 13:42:56 +0800

    can play video now, one at a time

--- multi_screen_player_demo.cpp
+++ multi_screen_player_demo.cpp
@@ -6,9 +6,63 @@ multi_screen_player_demo::multi_screen_player_demo(QWidget *parent) :
     ui(new Ui::multi_screen_player_demo)
 {
     ui->setupUi(this);
+
+    QAction *openAction = new QAction("&Open", this);
+    openAction->setShortcut(tr("Ctrl+O"));
+    connect(openAction,
+            &QAction::triggered,
+            this,
+            &multi_screen_player_demo::openFiles
+            );
+    addAction(openAction);
+
+    QAction *quitAction = new QAction("E&xit", this);
+    quitAction->setShortcut(tr("Ctrl+Q"));
+    connect(quitAction,
+            SIGNAL(triggered()),
+            qApp,
+            SLOT(quit())
+            );
+    addAction(quitAction);
+
+    setContextMenuPolicy(Qt::ActionsContextMenu);
+
+    mediaPlayer = new QMediaPlayer(this, QMediaPlayer::VideoSurface);
+    videoWidget = new QVideoWidget;
+    gridLayout = new QGridLayout;
+
+    mediaPlayer->setVideoOutput(videoWidget);
+    gridLayout->addWidget(videoWidget);
+    setLayout(gridLayout);
 }
 
 multi_screen_player_demo::~multi_screen_player_demo()
 {
     delete ui;
+}
+
+void multi_screen_player_demo::setUrl(const QUrl &url)
+{
+    setWindowFilePath(url.isLocalFile() ? url.toLocalFile() : QString());
+    mediaPlayer->setMedia(url);
+    mediaPlayer->play();
+}
+
+void multi_screen_player_demo::openFiles()
+{
+    QFileDialog fileDialog(this);
+    fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
+    fileDialog.setWindowTitle(tr("Open Files"));
+    fileDialog.setFileMode(QFileDialog::ExistingFiles);
+    QStringList supportedMimeTypes = mediaPlayer->supportedMimeTypes();
+    if (!supportedMimeTypes.isEmpty())
+        fileDialog.setMimeTypeFilters(supportedMimeTypes);
+    fileDialog.setDirectory(
+                QStandardPaths::standardLocations(QStandardPaths::MoviesLocation).value(
+                    0,
+                    QDir::homePath()
+                    ));
+    if (fileDialog.exec() == QDialog::Accepted) {
+        setUrl(fileDialog.selectedUrls().constFirst());
+    }
 }
--- multi_screen_player_demo.h
+++ multi_screen_player_demo.h
@@ -2,6 +2,9 @@
 #define MULTI_SCREEN_PLAYER_DEMO_H
 
 #include <QWidget>
+#include <QVideoWidget>
+#include <QtWidgets>
+#include <QMediaPlayer>
 
 namespace Ui {
 class multi_screen_player_demo;
@@ -15,8 +18,17 @@ public:
     explicit multi_screen_player_demo(QWidget *parent = nullptr);
     ~multi_screen_player_demo();
 
+public:
+    void setUrl(const QUrl &url);
+
+public slots:
+    void openFiles();
+
 private:
     Ui::multi_screen_player_demo *ui;
+    QMediaPlayer *mediaPlayer;
+    QGridLayout *gridLayout;
+    QVideoWidget *videoWidget;
 };
 
 #endif // MULTI_SCREEN_PLAYER_DEMO_H
